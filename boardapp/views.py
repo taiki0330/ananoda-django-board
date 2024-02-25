e1cb17fae45b23b2dd767485dadce39c70aff4f3
taiki0330 <jonas.taiki.0330@icloud.com>
2024-02-25 20:00:25 +0900

    first commit

---
+++ boardapp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+def signupfunc(request):
+    if request.method == 'POST':
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        try:
+            User.objects.get(username=username2)
+            return render(request, 'signup.html', {'error': 'このユーザーはすでに登録されています'})
+        except:
+            user = User.objects.create_user(username2, '', password2)
+            return render(request, 'signup.html', {'some': 100})
+    return render(request, 'signup.html', {'some': 100})
+
+def loginfunc(request):
+    if request.method == 'POST':
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        user = authenticate(request, username=username2, password=password2)
+        if user is not None:
+            login(request, user)
+            return redirect('signup')
+        else:
+            return redirect('login')
+    return render(request, 'login.html')
+
+def listfunc(request):
+    return render(request, 'list.html')
